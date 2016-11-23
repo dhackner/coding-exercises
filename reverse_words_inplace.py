8aa44991955d70f67e5de5ecf947f7edf10c2264
Daniel Hackner <dan.hackner@gmail.com>
2016-11-22 16:08:50 -0800

    Remove trimmed string assumption

--- reverse_words_inplace.py
+++ reverse_words_inplace.py
@@ -1,8 +1,6 @@
 # Strings are immutable in python, so switch to a character array and operate by
 # indexes
 
-# Assumption: Trimmed String
-
 # Algorithm: Create a function to reverse a word in place. Run on the full
 # string and then on each individual word
 
@@ -10,7 +8,17 @@
 def reverse(input_string):
     if input_string is None:
         return None
-    reversed_string_as_list = reverse_word(list(input_string))
+    string_as_list = list(input_string)
+
+    # Learning: When enumerating always remember the -1 to len
+    first_letter, last_letter = 0, len(string_as_list) - 1
+
+    while first_letter < len(string_as_list) and string_as_list[first_letter] == ' ':
+        first_letter += 1
+    # Learning: enumerate() is cleaner but trickier in an interview. Stay simple.
+    while last_letter > 0 and string_as_list[last_letter] == ' ':
+        last_letter -= 1
+    reversed_string_as_list = reverse_word(string_as_list, first_letter, last_letter)
     # print reversed_string_as_list
     index = 0
     while index < len(reversed_string_as_list):
@@ -42,6 +50,9 @@ def reverse_word(input_word, start=0, end=None):
 
 assert reverse_word("ABCDEF") == "FEDCBA"
 assert reverse("Dan Hackner wrote this") == "this wrote Hackner Dan"
+assert reverse("Dan Hackner wrote this ") == "this wrote Hackner Dan "
+assert reverse(" Dan Hackner wrote this ") == " this wrote Hackner Dan "
+assert reverse(" Dan Hackner wrote this") == " this wrote Hackner Dan"
 assert reverse("Dan") == "Dan"
 assert reverse("") == ""
 assert reverse(" ") == " "
