109cf8f887fc3d6d1aa800a01e41ed2b2cc7ee9e
Daniel Hackner <dan.hackner@gmail.com>
2016-11-22 16:26:34 -0800

    Cleanup and clarification

--- parenthesis_balancer.py
+++ parenthesis_balancer.py
@@ -3,31 +3,32 @@
 values = {'(': 1, ')': -1}
 
 
+# Base cases:
+# 1) negative total (more closes have happened then opens)
+# 2) no letter left, total == 0 (valid string)
+# 3) no letter left, total > 0 (invalid string due to hanging opens. this state
+# is allowed while there are still letters left)
 def balances(pre, post, total):
-    if total > 0:
-        return None
+    if total < 0:
+        # Learning: It is tempting to try to merge this case in with the one below. Spell out the definition of the base cases, so that you don't accidentally refactor them away.
+        # Done | Invalid combo due to having more closes at any point then opens
+        return []
 
     if len(post) == 0:
-        # Done
         if total == 0:
+            # Done | Valid combo
             return [pre]
         else:
-            return None
+            # Done | Invalid combo due to uneven open parens
+            return []
     if post[0] == '*':
-        returnList = []
-        balance = balances(pre, post[1:], total)
-        if balance is not None:
-            returnList += balance
-        balance = balances(pre, '(' + post[1:], total)
-        if balance is not None:
-            returnList += balance
-        balance = balances(pre, ')' + post[1:], total)
-        if balance is not None:
-            returnList += balance
-
-        return returnList
+        # Learning: Careful with array + vs array.append vs array.extend
+        return [] + \
+            balances(pre, post[1:], total) + \
+            balances(pre, '(' + post[1:], total) + \
+            balances(pre, ')' + post[1:], total)
     else:
-        return balances(pre + post[0], post[1:], total - values[post[0]])
+        return balances(pre + post[0], post[1:], total + values[post[0]])
 
 
 assert len(set(balances('', '(*(*)*)', 0))) == 5
